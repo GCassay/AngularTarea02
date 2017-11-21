6e69d46cbd019d85da621fa02dc6efd21c36873f
Gea Cassinello <geacassi@publicisgroupe.net>
2017-11-21 14:38:09 -0500

    Upload Project

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { CodeComponent } from './code/code.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TasksComponent,
+    CodeComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
